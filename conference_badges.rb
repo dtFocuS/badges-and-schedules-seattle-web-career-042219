5f40415413c6771313185b8b762f27ce00275702
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 23:09:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,10 +19,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array).each |str|
+  batch_badge_creator(array).each do |str|
     puts str
   end
-  assign_rooms(array).each |assignment|
+  assign_rooms(array).each do |assignment|
     puts assignment
   end
 end
