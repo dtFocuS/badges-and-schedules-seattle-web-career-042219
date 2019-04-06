92d0048afa38a1d4a3bc54ec1257380946a056fa
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 22:57:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch_badge = []
+  array.each do |name|
+    batch_badge.push(badge_maker(name))
+  end
+  batch_badge
+end
+
+def assign_rooms(array)
+  room_assignment = []
+  array.each_with_index {|name, i| puts "#{i}, #{name}"
+end
